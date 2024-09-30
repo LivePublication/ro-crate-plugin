48079497f2c5bff03b3645a9e40c1de15404518c
vhe20 <vhe20@uclive.ac.nz>
2024-09-30 23:57:03 +1300

    chore(cleanup): refactoring code and adding comments / TODO notes

---
+++ src/logic/pseudonym_manager.py
@@ -0,0 +1,42 @@
+"""
+Pseudonym Manager Module
+========================
+
+pseudodnym : a dictitious name;
+pseudonym : a unique identifier for an artifact that is generated automatically by the plugin.
+
+
+- The plugin shall automatically generate short, unique pseudonyms for each artifact upon detection.
+- The plugin shall ensure that each pseudonym is unique within the context of the current RO-Crate.
+- The plugin shall maintain a mapping between artifacts original names and their pseudonyms.
+- The plugin shall provide a user-friendly interface for users to view and search for artifacts using pseudonyms.
+
+This module manages pseudonym generation and mapping, ensuring uniqueness and providing user-friendly interfaces for viewing and searching artifacts using pseudonyms.
+interfaces for viewing and searching artifacts by pseduonym.
+
+CHECK
+
+========================
+"""
+# TODO : Implement the pseudonym manager module
+
+def generate_pseudonym(artifact):
+    '''Generates a pseudonym for the given artifact.'''
+    
+    pass
+
+def get_pseudonym(artifact):
+    '''Returns the pseudonym for the given artifact.'''
+    pass
+
+def get_artifact(pseudonym):
+    '''Returns the artifact for the given pseudonym.'''
+    pass
+
+def get_all_pseudonyms():
+    '''Returns all pseudonyms and their corresponding artifacts.'''
+    pass
+
+def search_pseudonyms(query):
+    '''Searches for pseudonyms that match the given query.'''
+    pass
--- src/logic/scanner.py
+++ src/logic/scanner.py
@@ -1,5 +1,4 @@
 """
-
 A function to allow the plugin to automatically scan the current directory for the
 presence of an RO-Crate instance.
 
@@ -10,7 +9,6 @@ presence of an RO-Crate instance.
 
 This file holds the logic for scanning the directory and detecting RO-Crate files. It
 also handles the notification to the user when an RO-Crate is detected.
-
 """
 import os
 import logging
@@ -19,7 +17,13 @@ logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
 def scanner(directory):
-    '''Scans the current working directory for RO-Crate files, will store the directoy paths.'''
+    """
+    Scans the given `directory` for RO-Crate files, and returns a list of
+    these directories as strings.
+    """
+    if directory is None:
+      logger.info("Error: provided directory is none")
+      return []
     logger.info(f"Scanning directory: {directory} for RO-Crate files.")
     paths = []
     for root, dirs, files in os.walk(directory):
@@ -28,69 +32,3 @@ def scanner(directory):
                 paths.append(root)
                 logger.info(f"RO-Crate detected in {root}.")
     return paths
-
-
-# class ROCrateDirectoryInformation():
-#     '''A class to get information about the RO-Crate directory.'''
-#     def __init__(self, folder):
-#         self.folder = folder
-#         self.files = self.get_files()
-#         self.images = self.get_images()
-
-#     def get_files(self):
-#         # gets the path of the folder to search
-#         folder_path = Path('src/ro-crates') / self.folder
-#         files = set()
-
-#         if not folder_path.exists():
-#             print("The folder given does not exist.")
-#             return files
-        
-#         # searches through the directory to get all files
-#         for file in folder_path.rglob('*'):
-#             if file.is_file():
-#                 files.add(file)
-
-#         return files
-
-#     def get_images(self):
-#         # a set of possible image extensions.
-#         image_extensions = { ".png", ".jpg", ".jpeg", ".gif", ".bmp", ".tiff", ".jfif" }
-
-#         # set of images present in the searched directory.
-#         images = {
-#             file for file in self.files if Path(file).suffix in image_extensions
-#         }
-
-#         return images
-    
-#     def get_images_names(self):
-#         return { image.name for image in self.images }
-
-
-# def main():
-#     print(ROCrateDirectoryInformation('ro-crate-with-images').get_images_names())
-
-
-# TODO make the scanner scan the entire directory of where you are, then store any folder's path that has an RO-Crate in it
-# (e.g. /ro-crate if /ro-crate has "ro-crate-metadata.json" in it then we store that path)
-
-# if __name__ == "__main__":
-#     # I'll neeed to import the validator, the validator could potentially be made a class (that would make it easier)
-#     # TODO: make the validator a class that validates all ROCrates and stores them as ROCrate instances 
-#     # TODO: create a setup.py file that will run the directory scanner on 'plugin' open, which then calls to the
-#     #       validator class to validate the given rocrates
-#     # TODO: once the validator has finished validating all of the rocrates, it should store them accordingly in  
-#     #      a .json file -> that will be cached locally :) note: use the platformdirs package (user cache dir (user_cache_dir))
-    
-#     import logic.validator as validator
-#     import platformdirs
-    
-#     paths = scanner(os.getcwd())
-    
-#     validator.setup()
-#     validator.validate_rocrate(paths[0])
-#     # TODO: ensure that the validated rocrates are made into a 
-    
-#     cache = platformdirs.user_cache_dir()
-#     print(cache)
